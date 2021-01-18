5b36c22eae82fb641d25c5ce7bbd037f6d27b0b6
Caroline Lynch <carolineablynch@gmail.com>
2021-01-18 18:45:42 -0500

    adding more inquirer prompts

--- index.js
+++ index.js
@@ -7,14 +7,38 @@ inquirer.prompt(
     [
         {
             type: 'input',
-            message="What is the title of the project?"
-            name:'title';
+            message="What is the title of the project?",
+            name:'title',
+            ///Validating user input, requiring an input from user 
             validate: (value) =>{ if(value){return true} else {return 'Please enter a value to continue'}},
         },
-
-
-
-        
+        {
+            type: 'input',
+            message="Please list the installation steps for your project.",
+            name:'Installation',
+            ///Validating user input, requiring an input from user 
+            validate: (value) =>{ if(value){return true} else {return 'Please enter a value to continue'}},
+        },
+        {
+            type: 'input',
+            message="Please enter the usage information for your project.",
+            name:'Usage',
+            ///Validating user input, requiring an input from user 
+            validate: (value) =>{ if(value){return true} else {return 'Please enter a value to continue'}},
+        },
+        {
+            type: 'input',
+            message="Please enter the contributing guidelines for your project.",
+            name:'Contributing',
+            ///Validating user input, requiring an input from user 
+            validate: (value) =>{ if(value){return true} else {return 'Please enter a value to continue'}},
+        },
+        {
+            type: 'input',
+            message="Please enter the test instructions for your project.",
+            name:'Testing',
+            ///Validating user input, requiring an input from user 
+            validate: (value) =>{ if(value){return true} else {return 'Please enter a value to continue'}},
+        }   
     ]
-
-}
+)
